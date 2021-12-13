b40d57b664c713eb65987303dcc935cc9756dedb
malyounis <synnyl@rams.colostate.edu>
2021-12-13 01:25:55 -0700

    12/2 lecture update

--- src/components/PhoneDetail.vue
+++ src/components/PhoneDetail.vue
@@ -1,11 +1,58 @@
 <template>
   <div>
-    <h1>Phone Detail</h1>
+    <div class="card">
+      <div class="card-body">
+        <h2 class="text-primary">Phone Preference Details</h2>
+        <br />
+        <p>
+          Color: <br /><strong>{{ phone.Color }}</strong>
+        </p>
+        <p>
+          Storage: <br /><strong>{{ phone.Storage }} GB</strong>
+        </p>
+        <p>
+          Screen Size: <br /><strong>{{ phone.ScreenSize }} inches</strong>
+        </p>
+        <p>
+          Price: <br /><strong>{{ formattedPrice }}</strong>
+        </p>
+      </div>
+    </div>
+    <br />
+    <router-link v-if="auth" :to="`/phones/${this.$route.params.pk}/preference`"
+      ><button class="btn btn-success">Add a Preference</button></router-link
+    >
+
+    <router-link v-else :to="`/login`"
+      ><button class="btn btn-outline-success">
+        Sign In to Add a Preference
+      </button></router-link
+    >
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    phone() {
+      let allPhones = this.$store.state.phones;
+
+      let thisPhone = allPhones.find((aPhone) => {
+        return aPhone.ProductTypePK[0] == this.$route.params.pk;
+      });
+      return thisPhone;
+    },
+    formattedPrice() {
+      return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(this.phone.Price);
+    },
+    auth() {
+      return this.$store.state.token;
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/components/Phones.vue
+++ src/components/Phones.vue
@@ -6,9 +6,24 @@
       <thead>
         <tr>
           <!-- has a title, pitch text, genre, empty section for details, like product/4 -->
+          <th>Color</th>
+          <th>Storage</th>
+          <th>Screen Size</th>
           <th></th>
         </tr>
       </thead>
+      <tbody>
+        <tr v-for="aPhone in thePhones" :key="aPhone.ProductPK">
+          <th>{{ aPhone.Color }}</th>
+          <th>{{ aPhone.Storage }}</th>
+          <th>{{ aPhone.ScreenSize }}</th>
+          <th>
+            <router-link :to="`/phones/${aPhone.ProductTypePK[0]}`"
+              ><button class="btn btn-primary">Details</button></router-link
+            >
+          </th>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
