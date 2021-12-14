6e85b54194eab06c9f1bc8bfc7c02870cde8de03
malyounis <synnyl@rams.colostate.edu>
2021-12-13 22:05:45 -0700

    small updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <div class="text-center">
+      <h1>Welcome!</h1>
+      <h5>
+        Here you can add in your preference for a phone and keep track of it.
+      </h5>
+
+      <img
+        src="../../photos/xiaomi_phones_a3-global_1x.png"
+        alt="phone-picture"
+        style="width: auto"
+      />
+      <h6>
+        If you would like to log in, <a href="/login"> click here</a>. If you
+        don't have an account yet, <a href="/signup">click here</a>.
+      </h6>
+    </div>
   </div>
 </template>
 
